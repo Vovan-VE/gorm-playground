e0380f0e4adfced59dd163167ec78f1e2e31bde8
Vovan-VE <vovan-ve@yandex.ru>
2026-03-19 15:30:40 +0800

    Test Model(A).FindInBatches(B)

--- main_test.go
+++ main_test.go
@@ -3,20 +3,39 @@ package main
 import (
 	"testing"
 
+	"gorm.io/gorm"
 	"gorm.io/playground/models"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: sqlite
+
+type UserExtended struct {
+	models.User
+	ExtraFoo int64
+}
 
 func TestGORM(t *testing.T) {
 	user := models.User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	err := DB.Create(&user).Error
+	if err != nil {
+		t.Errorf("Create failed: %v", err)
+	}
 
-	var result models.User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var results []UserExtended
+	err = DB.
+		// setting model for magic table name
+		Model(models.User{}).
+		Select(`*, 42 AS extra_foo`).
+		// taking results to different model with extra fields
+		FindInBatches(&results, 100, func(tx *gorm.DB, batch int) error {
+			// ...
+			return nil
+		}).
+		Error
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
